87a9076fb98b40ec040a1245b94ca837d1d5da97
JanekVodicka <vod.janek@gmail.com>
2024-10-04 15:42:59 +0200

    Update images.py on GitHub

--- images.py
+++ images.py
@@ -1,1 +1,11 @@
-# Imagesimport cv2# Read imageimg = cv2.imread('NY.jpg')# Show imagecv2.imshow('NY',img)# Keep window opencv2.waitKey()
+# Images
+import cv2
+
+# Read image
+img = cv2.imread('NY.jpg')
+
+# Show image
+cv2.imshow('NY',img)
+
+# Keep window open
+cv2.waitKey()
