37814ab198294386053053c8b30a78580691758d
Janek <vod.janek@gmail.com>
2024-10-04 14:44:55 +0200

    Commit 2

---
+++ for_loops.py
@@ -0,0 +1,1 @@
+# FOR LOOPS"""for .... in range()		- for loop probíhá v rámci list		- range() creates a sequence of numbers		- starting from 0 by default:		- range(75)[0, 1, 2, ....., 74]		- range(2, 75)[2, 3, 4, ....., 74] range(start_point, end_point)		- by default starts with 0			- last number not included"""
