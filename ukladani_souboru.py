b9d4ca8452af8c7759338b2b7da6ae8bde3346f2
JanekVodicka <vod.janek@gmail.com>
2024-10-04 15:42:07 +0200

    Update ukladani_souboru.py on GitHub

--- ukladani_souboru.py
+++ ukladani_souboru.py
@@ -1,1 +1,26 @@
-# TVORBA UKLÁDÁNÍ SOUBORŮimport os""" Textové soubory    # w = write    # r = read    # a = append - připojit    # + vytvoř, pokud není"""# Jméno souborufilename = "Textovy_soubor_zk.txt"file = open(filename,"w+")text = ("This is line new\n"        "This is also line \n"        "Ahoj \n"        "Haha \n")zk_input = input("Zadej číslo:")file.write(text + str(zk_input))file.close
+# TVORBA UKLÁDÁNÍ SOUBORŮ
+import os
+
+""" 
+Textové soubory
+    # w = write
+    # r = read
+    # a = append - připojit
+    # + vytvoř, pokud není
+"""
+
+# Jméno souboru
+filename = "Textovy_soubor_zk.txt"
+
+file = open(filename,"w+")
+
+text = ("This is line new\n"
+        "This is also line \n"
+        "Ahoj \n"
+        "Haha \n")
+
+zk_input = input("Zadej číslo:")
+
+file.write(text + str(zk_input))
+file.close
+
