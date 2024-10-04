78d7fbbde04c3852eda1dfeb1849e230890f7f42
Janek <vod.janek@gmail.com>
2024-10-04 15:13:33 +0200

    Upload module_first.py

---
+++ classes_main.py
@@ -0,0 +1,1 @@
+from classes_user import Userfrom classes_post import Postapp_user_1 = User("nn@nn.com", "Jan Vodicka", "pwd1", "DevOps Engineer")app_user_1.get_user_info()app_user_2 = User("jj@jj.com", "James Bond", "psw2", "Agent")app_user_2.get_user_info()new_post = Post("On a secret mission today", app_user_2.name)new_post.get_post_info()
---
+++ module_second.py
@@ -0,0 +1,1 @@
+import module_first# když spustím tento skript, výstupem je pouze jméno modulu, který jsem si naimportoval# V prvním modulu
